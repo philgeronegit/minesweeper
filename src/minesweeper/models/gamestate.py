3e0bef4d8de48df07422d24d67868fe124c0da20
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2024-09-11 14:21:59 +0200

    Update gamestate.py

--- src/minesweeper/models/gamestate.py
+++ src/minesweeper/models/gamestate.py
@@ -1,13 +1,10 @@
 import time
-from model.board import Board
+from models.board import Board
 
 # Here we initialize the game status based on the difficulty level
 
 class GameState:
     def __init__(self, difficulty="easy"):
-        """
-        Initialise l'état du jeu en fonction de la difficulté choisie.
-        """
         self.board = Board.from_difficulty(difficulty)  
         self.start_time = None  # Time to start the game
         self.win_time = None    # Indicate how many time the player put to win
@@ -22,16 +19,17 @@ class GameState:
         self.game_over = False
         self.game_won = False
         self.win_time = None
-        
 
 # Here we update the game status based on user input
 
     def is_game_over(self):
         return self.game_over
-    
+
+
+# Check that all unmined cells are revealed. If so, the game is won.
+
     def is_game_won(self):
         return self.game_won
-
 
 # The reveal function reveal the cell, check it safety  and check the click on a mine.
 # If the player clicks on a mine then the game is over and if not the adjacent celles are revealed
@@ -47,4 +45,27 @@ class GameState:
             case False:
                 self.board.reveal_cells(x, y)
                 return False
+
+
+    def check_victory(self):
+        if self.board.check_win():
+            self.game_over = True
+            self.game_won = True
+            self.win_time = time.time()  # Enregistre le moment où la victoire est obtenue
+            return True
+        return False
+
+#  Returns game duration in seconds.
+
+    def get_game_duration(self):
+        if self.start_time is None:
+            return 0
+        if self.game_won and self.win_time is not None:
+            return self.win_time - self.start_time  # Durée jusqu'à la victoire
+        return time.time() - self.start_time  # Durée depuis le début du jeu
+
+#   Returns the number of mines not found (miners yet to be discovered).
+
+    def get_remaining_mines(self):
+        return self.board.mines - self.board.flags
 
