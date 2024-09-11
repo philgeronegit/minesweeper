427c31bf81d65cf8653df18df34f3d1d171c0c05
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2024-09-11 12:38:22 +0200

    update GameState def reveal_cell

--- src/minesweeper/models/gamestate.py
+++ src/minesweeper/models/gamestate.py
@@ -17,7 +17,34 @@ class GameState:
 
 # Here we start the game and initialize the timer
 
-def start_game():
-    game_state = GameState()
-    game_state.start_time = time.time()
-    return game_state
+    def start_game(self):
+        self.start_time = time.time()
+        self.game_over = False
+        self.game_won = False
+        self.win_time = None
+        
+
+# Here we update the game status based on user input
+
+    def is_game_over(self):
+        return self.game_over
+    
+    def is_game_won(self):
+        return self.game_won
+
+
+# The reveal function reveal the cell, check it safety  and check the click on a mine.
+# If the player clicks on a mine then the game is over and if not the adjacent celles are revealed
+
+    def reveal_cell(self, x, y):
+        if self.game_over:
+            return
+        cell = self.board.cells[x][y]
+        match cell.is_mine:
+            case True:
+                self.game_over = True
+                return True
+            case False:
+                self.board.reveal_cells(x, y)
+                return False
+
